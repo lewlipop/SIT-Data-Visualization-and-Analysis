9ba83c7681a18dc550b136d02367bc94d6e74c83
Alexander <83382087+AlexanderLJX@users.noreply.github.com>
2024-01-23 14:20:56 +0800

    remove unused modules

--- googlemaps_scraper/scraper.py
+++ googlemaps_scraper/scraper.py
@@ -3,14 +3,12 @@ import threading
 import logging
 from concurrent.futures import ThreadPoolExecutor
 from selenium import webdriver
-from bs4 import BeautifulSoup
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.common.exceptions import NoSuchElementException, StaleElementReferenceException, TimeoutException, ElementClickInterceptedException
-from dateutil import parser
 from dateutil.relativedelta import relativedelta
 from datetime import datetime
 
